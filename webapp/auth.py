a3bf03820f10484d372e1a532e03c3c6a6449e1f
eladaspis <eladaspis@gmail.com>
2021-09-18 01:06:19 +0300

    adding sign-up page

--- webapp/auth.py
+++ webapp/auth.py
@@ -1,18 +1,32 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from flask.helpers import flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('base.html')
+    data = request.form
+    print(data)
+    return render_template("login.html", boolean=True)
 
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return render_template("logout.html")
 
 
-@auth.route('/sign_up')
+@auth.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
+    if request.method == 'POST':
+        data = request.form
+        email = data.get('email')
+        first_name = data.get('firstName')
+        password1 = data.get('password1')
+        password2 = data.get('password2')
+        if password1 != password2:
+            flash('Password isn\'t equal', category='error')
+        else:
+            flash('Account created!', category='success')
+
+    return render_template("sign_up.html")
